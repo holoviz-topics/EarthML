2ad8bfa5ffe68958e3ba22265dc66f3f2a68f787
Julia Signell <jsignell@gmail.com>
2018-11-21 08:52:52 -0500

    Trying to use doit

--- dodo.py
+++ dodo.py
@@ -3,3 +3,10 @@ if "PYCTDEV_ECOSYSTEM" not in os.environ:
     os.environ["PYCTDEV_ECOSYSTEM"] = "conda"
 
 from pyctdev import *  # noqa: api
+
+def task_before_test():
+    """Experimental: create test_catalog and use in place of real catalog"""
+    return {'actions':[
+        'python before_test.py',
+        'mv test_catalog.yml examples/catalog.yml'
+    ]}
