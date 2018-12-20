d1d7f24bfe09e555c164db24b7b1c414971ba0ed
Julia Signell <jsignell@gmail.com>
2018-12-20 15:12:58 -0500

    Ironing out some doit issues

--- dodo.py
+++ dodo.py
@@ -1,32 +1,9 @@
 import os
-import yaml
 
 if "PYCTDEV_ECOSYSTEM" not in os.environ:
     os.environ["PYCTDEV_ECOSYSTEM"] = "conda"
 
 from pyctdev import *  # noqa: api
-
-root_param = {
-    'name':'root',
-    'long':'path to root project dir',
-    'short': 'r',
-    'type': str,
-    'default': ''
-}
-path_param = {
-    'name':'path',
-    'long':'examples path',
-    'short': 'p',
-    'type': str,
-    'default': 'examples'
-}
-filename_param = {
-    'name':'filename',
-    'long':'catalog filename',
-    'short': 'f',
-    'type': str,
-    'default': 'catalog.yml'
-}
 
 
 def _prepare_paths(root, path, filename):
@@ -36,20 +13,21 @@ def _prepare_paths(root, path, filename):
     path = path if os.path.isabs(path) else os.path.join(root, path)
     return {
         'real': os.path.join(path, filename),
-        'test': os.path.join(path, 'data', '.data_stubs', filename)
-        'temp': os.path.join(path, 'tmp_', filename)
+        'test': os.path.join(path, 'data', '.data_stubs', filename),
+        'temp': os.path.join(path, 'tmp_' + filename),
     }
 
 
-def task_conda_setup():
-    """Experimental: set up conda with updated version, and yes set to always"""
+def task_ecosystem_setup():
+    """Set up conda with updated version, and yes set to always"""
     return {'actions': [
         "conda config --set always_yes True",
         "conda update conda",
     ]}
 
-def task_env_setup():
-    """Experimental: create environment from environment.yml and environment-dev.yml"""
+
+def task_env_create():
+    """Create environment from environment.yml and environment-dev.yml"""
     return {'actions': [
         "pip uninstall -y doit pyctdev",
         "conda env update -f environment.yml -n earthml",
@@ -57,63 +35,58 @@ def task_env_setup():
         "conda activate earthml",
     ]}
 
-def task_small_data_setup():
-    """Experimental: create catalog from real and stubs and substitute for real"""
 
-    def create_joined_catalog(root, path, filename):
+def task_small_data_setup():
+    """Experimental: Create catalog from real and stubs and substitute for real"""
+
+    def create_joined_catalog(root='', path='examples', filename='catalog.yml'):
+        import yaml
+
         paths = _prepare_paths(root, path, filename)
 
         # move real catalog file to temp if temp doesn't exist
         if os.path.exists(paths['temp']):
-            print('Temp file already exists')
+            print("Fail: Temp file already exists - try 'doit small_data_cleanup'")
             return
-
         os.rename(paths['real'], paths['temp'])
-        os.remove(paths['real'])
 
         # create joined version reading from temp and test
         with open(paths['temp']) as temp_cat:
             d = yaml.load(temp_cat)
             with open(paths['test']) as test_cat:
                 d['sources'].update(**yaml.load(test_cat)['sources'])
-            print('NEW CATALOG CONTENTS:', d)
 
-            with open(path['real'], mode='w') as real_cat:
-                real_cat.write(yaml.dump(d))
+                with open(paths['real'], mode='w') as real_cat:
+                    real_cat.write(yaml.dump(d))
+        print("Created catalog from real and stubs; substitute for real catalog.")
 
-    return {
-        'actions':[
-            CmdAction(create_joined_catalog),
-        ],
-        'params': [root_param, path_param, filename_param]}
+    return {'actions': [create_joined_catalog]}
+
 
 def task_small_data_cleanup():
     """Experimental: Replace the test version of the catalog with the real one"""
 
-    def remove_joined_catalog(root, path, filename):
+    def remove_joined_catalog(root='', path='examples', filename='catalog.yml'):
         paths = _prepare_paths(root, path, filename)
 
-        if os.path.exists(paths['temp']):
-            os.remove(paths['real'])
-            os.rename(paths['temp'], paths['real'])
+        if not os.path.exists(paths['temp']):
+            print("Nothing to do - no temp file found. Use git status to "
+                  "check that you have the real catalog at {}".format(paths['real']))
+            return
 
-    return {
-        'actions':[
-            CmdAction(remove_joined_catalog),
-        ],
-        'params': [root_param, path_param, filename_param]}
+        os.remove(paths['real'])
+        os.rename(paths['temp'], paths['real'])
+        print("Replaced the test version of the catalog with the real one.")
+
+    return {'actions': [remove_joined_catalog]}
+
 
 def task_build_website():
-    """Experimental: create test_catalog and use in place of real catalog"""
+    """Build website using nbsite"""
     return {'actions':[
         "nbsite generate-rst --org pyviz-topics --project-name earthml --offset 1 --nblink=top",
         "nbsite build --what=html --output=builtdocs",
-        "doit move_json"
-    ]}
-
-def task_move_json():
-    """Experimental: copy json from doc dir to builtdocs. This moved to nbsite"""
-    return {'actions':[
+        # The following commands will be part of the nbsite build cmd in nbsite >=0.5.2
         'cp doc/*.json builtdocs/ 2>/dev/null || :',
         'cp doc/topics/*.json builtdocs/topics/ 2>/dev/null || :',
         'cp doc/tutorial/*.json builtdocs/tutorial/ 2>/dev/null || :',
